d253dbfc3d55b22736bb2c4bce4d89ff53c90429
Omed0 <omedakram54@gmail.com>
2023-03-09 18:29:35 +0300

    second comit

---
+++ client/src/app/layout.jsx
@@ -0,0 +1,17 @@
+import '../styles/globals.css'
+
+export default function RootLayout({ children }) {
+
+  return (
+    <html lang="en">
+      <head>
+        <head />
+      </head>
+
+      <body>
+        {children}
+      </body>
+
+    </html>
+  )
+}
